8421e89d089a78bc3b1dd6b089e1a42bd1f4469a
Katin Maksim <katin_mi@inbox.ru>
2023-10-27 07:41:11 +0300

    Initial commit

---
+++ src/test/java/com/test/ScheduleTest.java
@@ -0,0 +1,6 @@
+package com.test;
+
+public class HeroesTest extends BaseTest{
+    private final static String URL = "https://mvnrepository.com/";
+    private final static String SEARCH_STRING = "https://sd.mstuca1.ru/";
+}
